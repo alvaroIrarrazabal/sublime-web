f4890d0e2e9d3eed93335727fa5c52e5969a017d
Alvaro Irarrazabal <irarrazabalalvaro@gmail.com>
2026-08-17 14:40:03 -0400

    feat: add reusable card component

---
+++ sublime-ui/src/app/shared/ui/card/card.component.ts
@@ -0,0 +1,18 @@
+import { Component, Input } from '@angular/core';
+
+@Component({
+  selector: 'app-card',
+  standalone: true,
+  imports: [],
+  templateUrl: './card.component.html',
+  styleUrl: './card.component.scss',
+})
+export class CardComponent {
+  @Input() title = '';
+
+  @Input() description = '';
+
+  @Input() image = '';
+
+ 
+}
