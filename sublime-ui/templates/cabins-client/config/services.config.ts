4497c77dbdb29d57104612773a82422188e2e2af
Alvaro Irarrazabal <irarrazabalalvaro@gmail.com>
2026-08-25 13:55:42 -0400

    refactor: generate clients from neutral template

---
+++ sublime-ui/templates/cabins-client/config/services.config.ts
@@ -0,0 +1,9 @@
+import { CabinService } from '../../../features/cabins/models/cabin-service.model';
+
+export const CABIN_SERVICES: CabinService[] = [
+  {
+    name: 'WiFi',
+    description: 'Conexión WiFi disponible durante la estadía.',
+    icon: 'wifi',
+  },
+];
